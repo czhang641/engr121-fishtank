a23f73fba05d642bdfce20b1aa372cf3799419cf
czhang641 <czhang641@gmail.com>
2018-02-20 16:24:05 -0500

    Add files via upload
    add basic heat

--- main.ino
+++ main.ino
@@ -18,7 +18,7 @@ int tempState = 0; // can be -2,-1,0,1,2. 2 means needs major corrections, 1 mea
 int sal = 0;
 int salState = 0; //same as temp state
 int SalTimeStart = 0;
-int SalTimeEnd =0;
+int SalTimeEnd = 0;
 int Valve1OpenTime = 0;
 int deadtime = 0;
 
@@ -31,7 +31,7 @@ void setup() {
   pinMode(8, OUTPUT); //relay output, volvo 2, salty?
   pinMode(9, OUTPUT); //relay output, thermresitor
   delay(100);
-  digitalWrite(7, HIGH);
+  digitalWrite(7, HIGH); //testing relays
   delay(50);
   digitalWrite(8, HIGH);
   delay(50);
@@ -51,29 +51,15 @@ void loop() {
     previousMillis = currentMillis;
     CheckTemp();
     CheckSaln();
-    if (abs(temp) > tempthreshold)
+    if (temp < 0) //put temp threshold here
     {
-      if (temp > 0)
+      if (temp > tempthreshold / 2)
       {
-        if (temp > tempthreshold / 2)
-        {
-          tempState = 2;
-        }
-        else
-        {
-          tempState = 1;
-        }
+        tempState = -2;
       }
-      if (temp < 0)
+      else
       {
-        if (temp > tempthreshold / 2)
-        {
-          tempState = -2;
-        }
-        else
-        {
-          tempState = -1;
-        }
+        tempState = -1;
       }
     }
     else
@@ -87,13 +73,13 @@ void loop() {
         if (sal > salnthreshold / 2)
         {
           salState = 2; //should do calculations for valve open
-          Valve1OpenTime = 5;//SalTimeCalc(); 
+          Valve1OpenTime = 5;//SalTimeCalc();
         }
         else
         {
           salState = 1;
           Valve1OpenTime = 5;//SalTimeCalc();
-        }        
+        }
       }
       if (sal < 0)
       {
@@ -116,57 +102,65 @@ void loop() {
   }
   if (salState == 2)          //Beginning of the salt correction block for to much salt
   {
-    digitalWrite(7,HIGH); //starts adding di cuz too much salt
+    digitalWrite(7, HIGH); //starts adding di cuz too much salt
     SalTimeStart = currentMillis; //set to when the valve is open
     Valve1OpenTime = 5;//SalTimeCalc();//run calc again for stage 1
     deadtime = 1000;//do dead time calc or if you want static idk what you people want
   }
-  if(salState == 2 && currentMillis == Valve1OpenTime+SalTimeStart) //check to stop valve open
+  if (salState == 2 && currentMillis == Valve1OpenTime + SalTimeStart) //check to stop valve open
   {
-    digitalWrite(7,LOW);//close valve
+    digitalWrite(7, LOW); //close valve
     salState = 1; //set state to 1
     SalTimeEnd = currentMillis;
     deadtime = 2000; //2 seconds
   }
-  if(salState == 1 && currentMillis == SalTimeEnd+deadtime)
+  if (salState == 1 && currentMillis == SalTimeEnd + deadtime)
   {
     Valve1OpenTime = 5;//SalTimeCalc();//run calc again for stage 1
-    digitalWrite(7,HIGH);
+    digitalWrite(7, HIGH);
     SalTimeStart = currentMillis; //set to when the valve is open
   }
-  if(salState == 1 && currentMillis == Valve1OpenTime+SalTimeStart) //stahp
+  if (salState == 1 && currentMillis == Valve1OpenTime + SalTimeStart) //stahp
   {
-    digitalWrite(7,LOW);//close valve
+    digitalWrite(7, LOW); //close valve
     salState = 0; //set state to 0
     SalTimeEnd = currentMillis;
     deadtime = 0; //set dead time to 0
   }
   if (salState == -2)          //Beginning of the salt correction block for too little salt
   {
-    digitalWrite(8,HIGH); //starts adding di cuz too much salt
+    digitalWrite(8, HIGH); //starts adding di cuz too much salt
     SalTimeStart = currentMillis; //set to when the valve is open
     Valve1OpenTime = 5;//SalTimeCalc();//run calc again for stage 1
     deadtime = 1000;//do dead time calc or if you want static idk what you people want
   }
-  if(salState == -2 && currentMillis == Valve1OpenTime+SalTimeStart) //check to stop valve open
+  if (salState == -2 && currentMillis == Valve1OpenTime + SalTimeStart) //check to stop valve open
   {
-    digitalWrite(8,LOW);//close valve
+    digitalWrite(8, LOW); //close valve
     salState = 1; //set state to 1
     SalTimeEnd = currentMillis;
     deadtime = 2000; //2 seconds
   }
-  if(salState == -1 && currentMillis == SalTimeEnd+deadtime)
+  if (salState == -1 && currentMillis == SalTimeEnd + deadtime)
   {
     Valve1OpenTime = 5;//SalTimeCalc();//run calc again for stage 1
-    digitalWrite(8,HIGH);
+    digitalWrite(8, HIGH);
     SalTimeStart = currentMillis; //set to when the valve is open
   }
-  if(salState == -1 && currentMillis == Valve1OpenTime+SalTimeStart) //stahp
+  if (salState == -1 && currentMillis == Valve1OpenTime + SalTimeStart) //stahp
   {
-    digitalWrite(8,LOW);//close valve
+    digitalWrite(8, LOW); //close valve
     salState = 0; //set state to 0
     SalTimeEnd = currentMillis;
     deadtime = 0; //set dead time to 0
+  }
+  if(temp > tempthreshold)
+  {
+    digitalWrite(9,LOW);
+  }
+  if(temp < tempthreshold)
+  {
+    digitalWrite(9,HIGH);
   }
 }
 
@@ -185,6 +179,11 @@ void CheckSaln()
 void SalTimeCalc()
 {
   //math
+  delay(1);
+  return;
+}
+void HeatTimeCalc()
+{
   delay(1);
   return;
 }
